5bf2b8a65c3a5cb45b335ccc80b5d046f7cd1736
calint <calin.tenitchi@gmail.com>
2017-06-15 12:47:50 +0700

    .

--- howto/cloud/linode/linode-install.sh
+++ howto/cloud/linode/linode-install.sh
@@ -1,9 +1,10 @@
 #!/bin/bash -x
 IP=$1
 NM=${2:-baba-chess-trainer}
+GZ=~/$NM.gz
 GIT=http://github.com/calint/$NM
 
-echo " * init linode ubuntu 16 lts at $IP using $GIT" &&
+echo " * init linode ubuntu 16 lts at $IP" &&
 echo "     from $GIT" &&
 
 echo " * copy ssh public key to root" &&
@@ -11,33 +12,20 @@ ssh-copy-id root@$IP &&
 
 echo " * install java 8" &&
 #ssh root@$IP "apt-get update && apt-get -y upgrade && apt-get -y install openjdk-8-jre-headless && git clone $GIT /$NM" &&
-ssh root@$IP apt-get -y install openjdk-8-jre-headless &&
+ssh root@$IP "apt-get update && apt-get -y install openjdk-8-jre-headless git" &&
+
+#echo " * install app $NM" &&
+#ssh root@$IP "git clone $GIT /$NM" &&
 
 echo " * install app $NM" &&
-ssh root@$IP git clone $GIT /$NM &&
-
-exit
-
+scp $GZ root@$IP:/ &&
+ssh root@$IP "cd / && tar -xzvf $NM.gz -C /" &&
 
 echo " * copy systemd service file ba.service to /etc/systemd/system/" &&
 scp ba.service root@$IP:/etc/systemd/system/ &&
+
+echo " * enable service" &&
 ssh root@$IP systemctl enable ba &&
-#echo " * update packages" &&
-#ssh root@$IP apt-get -y upgrade &&
-
-#echo " * install java runtime" &&
-#ssh root@$IP apt-get -y install openjdk-8-jre-headless unzip &&
-
-#echo " * copy install image  $IMG to $IP:/" &&
-#scp $IMG root@$IP:/
-
-#ssh root@$IP git clone $GIT $NM
-
-#echo " * unpack image to /" &&
-#ssh root@$IP tar xzf $IMG -C / && 
-
-#echo " * adjust installed image" &&
-#ssh root@$IP chmod +x /a/ba.sh && 
 
 echo " * reboot" &&
 ssh root@$IP reboot && 
